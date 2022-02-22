225c62e007476ce8d3ec524e0ac16947148b3ee9
Victor Umunna <umunnavictor01@gmail.com>
2022-02-22 14:16:26 +0100

    update

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ def main():
     st.write("""### We need some information to predict your chances of admission""")
 
 
-    gre_score = st.text_input("GRE Score","Type Here")
-    toefl_score = st.text_input("TOEFL Score","Type Here")
-    university_rating = st.slider("University Rating", 1, 5, 1)
-    statement_of_purpose_score = st.text_input("Statement of Purpose Score","Type Here")
-    letter_of_recommendation_score = st.text_input("Letter of Recommendation Score","Type Here")
-    cgpa = st.text_input("CGPA","Type Here")
+    gre_score = st.number_input("GRE Score", min_value=0, max_value=340, value=0)
+    toefl_score = st.number_input("TOEFL Score", min_value=0, max_value=120, value=0)
+    university_rating = st.slider("University Rating", 1, 5, 1) 
+    statement_of_purpose_score = st.number_input("Statement of Purpose Score", min_value=0.0, max_value=5.0, value=0.0)
+    letter_of_recommendation_score = st.number_input("Letter of Recommendation Score", min_value=0.0, max_value=5.0, value=0.0)
+    cgpa = st.number_input("CGPA", min_value=0.00, max_value=10.00, value=0.00)
     research = st.selectbox('Research?  1 = Yes , 0 = No',(1, 0,))
     if research == 1:
         st.write('You selected: Yes')
@@ -41,7 +41,7 @@ def main():
         percentage = score * 100
         percentage = round(percentage[0])
 
-        st.subheader(f"You have a {percentage}% chance of getting admission")
+        st.subheader(f"You have {percentage}% chance of getting admission")
 
 if __name__=='__main__':
     main()
